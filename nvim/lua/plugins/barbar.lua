75deb63405bdcf130646dac4a006073d55cde1ce
Kamil Kseń <kamyilksen@gmail.com>
2024-09-24 11:59:43 +0200

    Moved search keymaps from s to f and configured mason ensure_installed

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -66,14 +66,14 @@ return {
       ---- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
       --NvimTree = true,
       ---- Or, specify the text used for the offset:
-      --undotree = {
-      --  text = 'undotree',
-      --  align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
-      --},
+      undotree = {
+        text = 'undotree',
+        align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+      },
       ---- Or, specify the event which the sidebar executes when leaving:
       ['neo-tree'] = { event = 'BufWipeout' },
       ---- Or, specify all three
-      --Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+      Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
     },
   },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
--- nvim/lua/plugins/lsp-lines.lua
+++ nvim/lua/plugins/lsp-lines.lua
@@ -2,21 +2,5 @@
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   event = "LspAttach",
-  dependencies = {
-    {
-      "AstroNvim/astrocore",
-      opts = {
-        mappings = {
-          n = {
-            ["<Leader>uD"] = { function() require("lsp_lines").toggle() end, desc = "Toggle lsp-lines (better located messages)" },
-          },
-        },
-
-        diagnostics = {
-          virtual_text = false,
-        },
-      },
-    },
-  },
   opts = {},
 }
