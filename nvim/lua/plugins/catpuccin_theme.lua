cae7df681e0f7fd5e1f55fe12aee99f1107b2cda
Kamil Kseń <kamyilksen@gmail.com>
2024-09-25 21:53:38 +0200

    Disabled all italics and bolds 2

--- nvim/lua/plugins/catpuccin_theme.lua
+++ nvim/lua/plugins/catpuccin_theme.lua
@@ -18,38 +18,39 @@ return {
       light = 'latte',
       dark = 'mocha',
     },
-    transparent_background = false, -- disables setting the background color.
+    transparent_background = true, -- disables setting the background color.
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
       enabled = true, -- dims the background color of inactive window
       shade = 'dark',
       percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
+    no_italic = true, -- Force no italic
+    no_bold = true, -- Force no bold
     no_underline = false, -- Force no underline
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-      comments = {}, -- Change the style of comments
-      conditionals = {},
-      loops = {},
-      functions = {},
-      keywords = {},
-      strings = {},
-      variables = {},
-      numbers = {},
-      booleans = {},
-      properties = {},
-      types = {},
-      operators = {},
-      -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      comments = { 'NONE' }, -- This removes italics from comments
+      conditionals = { 'NONE' },
+      loops = { 'NONE' },
+      functions = { 'NONE' },
+      keywords = { 'NONE' },
+      strings = { 'NONE' },
+      variables = { 'NONE' },
+      numbers = { 'NONE' },
+      booleans = { 'NONE' },
+      properties = { 'NONE' },
+      types = { 'NONE' },
+      operators = { 'NONE' },
     },
-    color_overrides = {},
-    custom_highlights = {},
+    custom_highlights = {
+      Comment = { style = 'NONE' }, -- Disable italics in comments
+      Function = { style = 'NONE' }, -- Disable italics in function names
+      Keyword = { style = 'NONE' }, -- Disable italics in keywords
+    },
     default_integrations = false,
     integrations = {
       cmp = true,
-      gitsigns = true,
       treesitter = true,
       notify = true,
       telescope = false,
