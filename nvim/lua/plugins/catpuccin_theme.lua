64cabc0f91775d65d5ca050bd21e43a8ed7f7744
Kamil Kseń <kamyilksen@gmail.com>
2024-09-25 08:33:30 +0200

    Added Git Blame + fixed Catppuccin theme for barbar

--- nvim/lua/plugins/catpuccin_theme.lua
+++ nvim/lua/plugins/catpuccin_theme.lua
@@ -3,6 +3,15 @@ return {
   name = 'catppuccin',
   lazy = true,
   priority = 1000,
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'catppuccin-mocha'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
   setup = {
     flavour = 'mocha', -- latte, frappe, macchiato, mocha
     background = { -- :h background
