e8ec6e4f3bdfad1201a0402a871a6f94a72cf132
Kamil Kseń <kamil@macbook-pro-kamil-1.home>
2025-01-11 21:44:15 +0100

    Change sketchybar a little bit

--- sketchybar/items/calendar.sh
+++ sketchybar/items/calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sketchybar --add item calendar right \
-           --set calendar icon=􀧞  \
+           --set calendar icon=  \
                           update_freq=30 \
                           script="$PLUGIN_DIR/calendar.sh"
