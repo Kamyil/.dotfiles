c783037ea526fd338c240a2de51d720a2304941d
Kamil Kseń <kamil@MacBook-Pro-Kamil.local>
2025-01-14 13:25:51 +0100

    Add active app icon to sketchybar

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sketchybar --set $NAME label="$(date +'%a %d %b %I:%M %p')"
+# sketchybar --set $NAME label="$(date -R +'%A %D %B %I:%M %p' )"
+sketchybar --set "$NAME" label="$(LC_TIME=pl_PL.UTF-8 date +'%A, %d %B %Y, %H:%M')"
