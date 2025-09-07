af5f06d98bab1ac00cbedc90e4736682fbc20380
Kamil Ksen <mccom_kks@mccom.pl>
2025-09-07 09:14:52 +0200

    Add NixOS config

---
+++ nixos/configuration.nix
@@ -0,0 +1,164 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  programs.hyprland.enable = true; # ships Hyprland + Xwayland
+
+
+  
+  
+  users.groups.kamil = {};
+  programs.zsh.enable = true;
+users.users.kamil = {
+  shell = pkgs.zsh;
+  isNormalUser = true;
+  group = "kamil";          # Add this line
+  description = "kamil";
+  extraGroups = [ "networkmanager" "wheel" ];
+  packages = with pkgs; [];
+};
+
+  services.xserver.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.desktopManager.gnome.enable = false; # just GDM
+
+  services.greetd.enable = true;
+  services.greetd.settings = {
+   default_session = {
+     command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
+   };
+  };
+
+
+  # SPICE guest agent for clipboard sharing betwen host and vm
+  services.spice-vdagentd.enable = true;
+
+  # Sreenshare, filepickers etc. (desktop portals)
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+  ];
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Enable OpenGL GPU acceleration
+  hardware.graphics.enable = true;
+
+  boot.initrd.kernelModules = ["virtio_gpu"];
+  services.xserver.videoDrivers = [ "virtio" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+	
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   wget
+   curl
+   git
+   pkg-config
+   openssl
+   gnumake
+
+   vivaldi
+
+   # rg delta bat btop
+
+   # Terminals
+   ghostty
+   wezterm
+   kitty
+
+
+   # Others
+   hyprpaper hypridle hyprlock
+   waybar wofi wl-clipboard grim slurp
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ nixos/flake.nix
@@ -0,0 +1,158 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager/release-24.11";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+# bring your dotfiles in as a flake input (read-only, pure)
+		dotfiles.url = "path:..";
+		dotfiles.flake = false;
+
+		neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay"; 
+		rust-overlay.url = "github:oxalica/rust-overlay";
+
+# Add private fonts
+		berkeley-font = {
+			url = "path:///home/kamil/.local/share/fonts";
+			flake = false;
+		};
+
+	};
+
+	outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, berkeley-font, neovim-nightly-overlay, dotfiles, rust-overlay, ... }:
+		let
+		system = "aarch64-linux"; # UTM on M1/M2
+		pkgs = import nixpkgs {
+			inherit system;
+			overlays = [rust-overlay.overlays.default ];
+			config.allowUnfree = true;
+		};
+	pkgsUnstable = import nixpkgs-unstable {
+		inherit system;
+		config.allowUnfree = true;
+	};
+
+	lib = nixpkgs.lib;
+
+	in {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			inherit system;
+			specialArgs = { inherit pkgs; };
+			modules = [
+				./configuration.nix
+
+# enable HM as a NixOS module
+					home-manager.nixosModules.home-manager
+					{
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
+
+# --- your user ---
+						home-manager.users.kamil = { pkgs, config, ... }: {
+							home.username = "kamil";
+							home.homeDirectory = lib.mkForce "/home/kamil";
+							home.stateVersion = "24.11";
+
+
+# if mako comes in, disable it since it crashes
+# home.disabledModules = ["services.mako.nix"];
+
+# packages you want at user scope
+							home.packages = (with pkgs; [
+									wezterm firefox
+									fzf bat delta lazygit lazydocker docker
+
+
+# C, Rust, Zig
+									gcc 
+									(pkgs.rust-bin.nightly.latest.default.override {
+									 extensions = [ "rust-src" "cargo" "rustc" ];
+									 })
+							]) ++
+								[
+# + the ones from `unstable` branch
+								pkgsUnstable.opencode
+									neovim-nightly-overlay.packages.${system}.default
+								];
+
+
+# fonts.fontconfig.defaultFonts.monospace = ["Berkeley Mono"];
+
+# programs.*: let HM manage app configs
+
+							programs.zsh = {
+								enable = true;
+								autosuggestion.enable = true;
+								syntaxHighlighting.enable = true;
+								initExtra = ''
+									source ~/.zshrc
+									'';
+							};
+
+
+
+
+# programs.starship = {
+# 	enable = true;
+# 	enableZshIntegration = true;
+# };
+
+							programs.git = {
+								enable = true;
+								userName = "Kamil Ksen";
+								userEmail = "mccom_kks@mccom.pl";
+								extraConfig = {
+									core.editor = "nvim";
+									init.defaultBranch = "main";
+								};
+							};
+
+# --- map your dotfiles repo into place ---
+# ~/.config/*
+	      	  xdg.configFile."nvim".source     = dotfiles + "/nvim";
+	      	  xdg.configFile."wezterm".source  = dotfiles + "/wezterm";
+# xdg.configFile."starship.toml".source = dotfiles + "/starship/starship.toml";
+xdg.configFile."hyprland".source     = dotfiles + "/config/hypr";
+# (you also have mac-only stuff like yabai/skhd/sketchybar—gate those below)
+
+home.file."second-brain/.keep".text = ""; 
+
+# files in $HOME root (start with a dot)
+home.file.".local/share/fonts/BerkeleyMono-Regular.otf".source = berkeley-font + "/BerkeleyMono-Regular.otf";
+home.file.".local/share/fonts/BerkeleyMono-Bold.otf".source = berkeley-font + "/BerkeleyMono-Bold.otf";
+home.file.".local/share/fonts/BerkeleyMono-Oblique.otf".source = berkeley-font + "/BerkeleyMono-Oblique.otf";
+home.file.".local/share/fonts/BerkeleyMono-Bold-Oblique.otf".source = berkeley-font + "/BerkeleyMono-Bold-Oblique.otf";
+
+fonts.fontconfig.enable = true;
+fonts.fontconfig.defaultFonts.monospace = [ "Berkeley Mono" ];
+
+# alacritty examples (you have both toml & yml in repo)
+# home.file.".alacritty.toml".source = dotfiles + "/.alacritty.toml";
+# or choose one and ignore the other
+
+# optional: run your old bootstrap once on activation (idempotent)
+# home.activation.bootstrap = lib.hm.dag.entryAfter ["writeBoundary"] ''
+#   # e.g., migrate/clean legacy symlinks if needed
+# '';
+
+# Nice defaults
+xdg.enable = false;
+programs.home-manager.enable = true;
+};
+
+# Per-OS gating for files you only want on macOS / Linux
+# Example: only install yabai/skhd configs on macOS
+# (for later if you reuse the same flake on your Mac)
+# home-manager.sharedModules = [
+#   ({ pkgs, ... }: lib.mkIf pkgs.stdenv.isDarwin {
+#     xdg.configFile."yabai".source = dotfiles + "/yabai";
+#     xdg.configFile."skhd".source  = dotfiles + "/skhd";
+#   })
+# ];
+}
+];	
+};
+};
+}
+
