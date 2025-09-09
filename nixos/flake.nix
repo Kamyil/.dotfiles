078053a26076dbe72893361c6cc5fac3b468b42c
Kamil Ksen <mccom_kks@mccom.pl>
2025-09-09 18:32:45 +0200

    Stay on NixOS-24.11 since it doesn't break

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
