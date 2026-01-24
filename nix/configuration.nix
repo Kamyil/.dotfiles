605c291e5c4de778fc8f42f2c0e1f0c1aed0f4ba
Kamil Ksen <mccom_kks@mccom.pl>
2026-01-24 21:16:24 +0100

    update configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -104,6 +104,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
     wget
     curl
     git
