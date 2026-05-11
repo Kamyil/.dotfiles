f5d41f8f9100b7c0ae8639805787ac925345d587
Kamil Ksen <mccom_kks@mccom.pl>
2026-05-11 07:50:45 +0200

    (hunkdiff) working hunkdiff inside lazygit <3

--- nix/overlays/hunkdiff.nix
+++ nix/overlays/hunkdiff.nix
@@ -6,23 +6,23 @@
 final: prev:
 
 let
-  version = "0.10.0";
+  version = "0.12.0-beta.1";
 
   platformInfo = if prev.stdenv.isDarwin then
     if prev.stdenv.isAarch64 then {
       binaryPkg = "hunkdiff-darwin-arm64";
-      binaryHash = "09hgb7yzvwsfpp3gxpjzvgjf85njak7p8j0mgp5nmngdadwf62f3";
+      binaryHash = "04b1yzfm6lmhcimj4a2dc9h90mw5h22sck6pw6jssxy7xa6xqd15";
     } else {
       binaryPkg = "hunkdiff-darwin-x64";
-      binaryHash = "13r8vygzqwvq2npakgk8cmkljq0h0pdnp60wxxnc3zpkqcxsgw9c";
+      binaryHash = "0hq2c4zrnv47v3y6qdxiy53zsxz2di7dkail3klpz82gxgwlsdcj";
     }
   else
     if prev.stdenv.isAarch64 then {
       binaryPkg = "hunkdiff-linux-arm64";
-      binaryHash = "0phq4z67qif8i5h2lwrklm6a14v97q4zdwrh96c2m4r62xbywpzv";
+      binaryHash = "0i53ibwbqp1sabg6mry2vbmnw0s3v6pyv09dl17aabrqhv7aljs5";
     } else {
       binaryPkg = "hunkdiff-linux-x64";
-      binaryHash = "083xvh247x06g56fdfg05a2w6lz6rxyh8m9v60rvfpxw59w1wa90";
+      binaryHash = "1rarsxidpszrs5pr818883d9z605c21371xlzyc9khshpyib0ard";
     };
 in
 
@@ -33,7 +33,7 @@ in
 
     src = prev.fetchurl {
       url = "https://registry.npmjs.org/hunkdiff/-/hunkdiff-${version}.tgz";
-      sha256 = "05mnjgd0b7c13jh1ry8q1qcsxlyl5ygwdqd2z36a64wzpiq7vsn0";
+      sha256 = "0ppqjdrbkvfy5ns5ir6cl5iv8sy5003czz0g3psc9p428dajgm3w";
     };
 
     nativeBuildInputs = [ prev.makeWrapper ];
