8e699263ca5577dd50f2c54cdc5d9052663adc1e
Kamil Ksen <mccom_kks@mccom.pl>
2026-06-10 10:33:52 +0200

    (theme) refine kanagawa paper terminal colors

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -193,6 +193,7 @@ in
             ".config/omarchy/current/theme/waybar.css".source = link "config/omarchy/current/theme/waybar.css";
             ".config/omarchy/current/background".source = link "Downloads/2-Pawel-Czerwinski-Abstract-Purple-Blue.jpg";
             ".config/tmux".source = link "config/tmux";
+            ".config/lsd".source = link "config/lsd";
             ".config/alacritty".source = link "config/alacritty";
             ".config/walker/config.toml".source = link "config/walker/config.toml";
             ".config/walker/themes/kanagawa.css".source = link "config/walker/themes/kanagawa.css";
