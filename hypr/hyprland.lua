91d9fda06f9b606ba674da164af0c993f6cba676
Kamil Ksen <mccom_kks@mccom.pl>
2026-07-28 14:29:08 +0200

    fix(xkb): change left alt to be AltGr (in order to be able to type polish letters)

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -19,7 +19,7 @@ hl.config({
 
 	input = {
 		kb_layout = "pl",
-		kb_options = "caps:escape",
+		kb_options = "caps:escape,lv3:lalt_switch",
 		follow_mouse = 1,
 		repeat_rate = 50,
 		repeat_delay = 300,
