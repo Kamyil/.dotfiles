180711f4cf6fab04f99aa5a9c9b326c261faeb13
Kamil Ksen <mccom_kks@mccom.pl>
2026-09-01 11:21:25 +0200

    change(nixos): replace thunar with nautilus

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -105,7 +105,7 @@ in
               docker-compose
               satty
               chromium
-              thunar
+              nautilus
               ffmpegthumbnailer
               imv
               mpv
@@ -141,7 +141,7 @@ in
             ];
 
           programs.zsh.shellAliases = {
-            finder = "thunar";
+            finder = "nautilus";
             nrs = "nh os switch ~/.dotfiles/nix";
             transcribe = "parakeet-transcribe";
           };
